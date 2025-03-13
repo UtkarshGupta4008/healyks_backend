558f96d89ee6471b9f2230ab3c73ffe6c886236f
Utkarsh Gupta <guptautkarsh452@gmail.com>
2025-03-14 00:18:15 +0530

    fix- minor fixes

--- src/models/user.js
+++ src/models/user.js
@@ -47,7 +47,8 @@ const userSchema = new mongoose.Schema({
     },
     physicalActivity: {
       type: String,
-      enum: ['Daily','Weakly','Monthly']
+      enum: ['Daily','Weakly','Monthly'],
+      default: 'None'
     }
   },
   /*
